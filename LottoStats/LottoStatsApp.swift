14f43ab1afe69bbd5f6a74cee4d93207ed9ac28a
Rafal Mizera <r.mizera96@gmail.com>
2026-05-12 02:11:29 +0200

    Initial Commit

---
+++ LottoStats/LottoStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoStatsApp.swift
+//  LottoStats
+//
+//  Created by Rafal Mizera on 12/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LottoStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
